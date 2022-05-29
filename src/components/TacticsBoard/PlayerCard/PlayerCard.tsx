34c9efb5c897f98e27357046ed904070e3017fd7
Raivis <raivis.joksts@jeff-app.com>
2022-05-30 00:03:10 +0300

    update player card

--- src/components/TacticsBoard/PlayerCard/PlayerCard.tsx
+++ src/components/TacticsBoard/PlayerCard/PlayerCard.tsx
@@ -1,34 +1,41 @@
+import { DnDWrapper } from "../DnDWrapper";
 import React from "react";
 import { ReactComponent as ProfileSvg } from "../../../assets/svgs/profile.svg";
 import { Player } from "../../../utility/types";
 
 import classes from "./PlayerCard.module.css";
 
-const PlayerCard: React.FC<{ player: Player }> = ({ player }) => (
-  <div className={classes.container}>
-    <div className={classes.jerseyWrapper}>
-      <img
-        src={
-          player.isGk
-            ? "/assets/images/gk-jersey.png"
-            : "/assets/images/player-jersey.png"
-        }
-        alt="jersey"
-        className={classes.jersey}
-      />
-      <div className={classes.jerseyNumber}>{player.jerseyNumber}</div>
-    </div>
-    <div className={classes.playerInfo}>
-      <div className={classes.playerPosStats}>
-        <div className={classes.circle}></div>
-        <div className={classes.position}>{player.playerPosition.position}</div>
+const PlayerCard: React.FC<{ player: Player }> = ({ player }) => {
+  return (
+    <DnDWrapper id={player.id}>
+      <div className={classes.container}>
+        <div className={classes.jerseyWrapper}>
+          <img
+            src={
+              player.isGk
+                ? "/assets/images/gk-jersey.png"
+                : "/assets/images/player-jersey.png"
+            }
+            alt="jersey"
+            className={classes.jersey}
+          />
+          <div className={classes.jerseyNumber}>{player.jerseyNumber}</div>
+        </div>
+        <div className={classes.playerInfo}>
+          <div className={classes.playerPosStats}>
+            <div className={classes.circle}></div>
+            <div className={classes.position}>
+              {player.playerPosition.position}
+            </div>
+          </div>
+          <div className={classes.playerPersonalInfo}>
+            <ProfileSvg className={classes.profile} />
+            <div className={classes.playerName}>{player.name.firstName}</div>
+          </div>
+        </div>
       </div>
-      <div className={classes.playerPersonalInfo}>
-        <ProfileSvg className={classes.profile} />
-        <div className={classes.playerName}>{player.name.firstName}</div>
-      </div>
-    </div>
-  </div>
-);
+    </DnDWrapper>
+  );
+};
 
 export default PlayerCard;
