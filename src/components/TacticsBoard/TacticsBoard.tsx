f82a607ae9e5b86957314c5851857da8c959dd90
Raivis <raivis.joksts@jeff-app.com>
2022-05-30 00:04:38 +0300

    update tactics borad

--- src/components/TacticsBoard/TacticsBoard.tsx
+++ src/components/TacticsBoard/TacticsBoard.tsx
@@ -1,22 +1,33 @@
+import { DndContext } from "@dnd-kit/core";
+import { PositionGrid } from "./PositionGrid";
 import { Fragment } from "react";
 import { team1 } from "../../data";
 import Pitch from "./Pitch/Pitch";
 import Substitute from "./Substitute/Substitute";
 import classes from "./TacticsBoard.module.css";
 
-const TacticsBoard: React.FC = () => (
-  <div className={classes.container}>
-    <div className={classes.board}>
-      <Pitch />
+const TacticsBoard: React.FC = () => {
+  return (
+    <div className={classes.boardContainer}>
+      <div className={classes.board}>
+        <Pitch />
+      </div>
+      <div className={classes.boundedArea}>
+        <DndContext>
+          <div className={classes.gridContainer}>
+            <PositionGrid firstEleven={team1.firstEleven} />\
+          </div>
+          <div className={classes.subsCol}>
+            {team1.subs.map((player, idx) => (
+              <Fragment key={player.id}>
+                <Substitute player={player} />
+              </Fragment>
+            ))}
+          </div>
+        </DndContext>
+      </div>
     </div>
-    <div className={classes.subsCol}>
-      {team1.subs.map((player) => (
-        <Fragment key={player.id}>
-          <Substitute player={player} />
-        </Fragment>
-      ))}
-    </div>
-  </div>
-);
+  );
+};
 
 export default TacticsBoard;
