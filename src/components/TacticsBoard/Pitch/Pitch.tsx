bfb7a8ec61a8bdd70d2264e1278f7989c8e14e6d
Raivis <raivis.joksts@jeff-app.com>
2022-05-30 00:03:20 +0300

    update pitch

--- src/components/TacticsBoard/Pitch/Pitch.tsx
+++ src/components/TacticsBoard/Pitch/Pitch.tsx
@@ -1,7 +1,6 @@
 import Background from "./Background/Background";
 import classes from "./Pitch.module.css";
-import { team } from "../../../utility/constants";
-console.log(team);
+
 const Pitch: React.FC = () => (
   <div className={classes.container}>
     <Background />
